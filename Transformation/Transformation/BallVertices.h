64ebdfa50ec6a65950838e8a063e72bffd731116
Mars_i <dwellerofuniverse@gmail.com>
2018-03-31 14:25:50 +0800

    绘制立方体和球体并且进行变换

---
+++ Transformation/Transformation/BallVertices.h
@@ -0,0 +1,45 @@
+#include "imgui/imgui.h"
+#include <iostream>
+#include <algorithm>  
+#include <glew/glfw3.h>
+#include <math.h>
+#include <vector>
+#define PI 3.1415926  
+using namespace std;
+int genBallVertices(float* &ballVertices, float radius, int stack, int slice) {
+	float stackStep = (float)(PI / stack);
+	float sliceStep = (float)(PI / slice);
+	float r0, r1, x0, x1, y0, y1, z0, z1;
+	float alpha0 = 0, alpha1 = 0;
+	float beta = 0;
+	int index = 0;
+	vector<float> coodlist;
+	for (int i = 0; i < stack; i++) {
+		alpha0 = (float)(-PI / 2 + (i*stackStep));
+		alpha1 = (float)(-PI / 2 + ((i + 1)*stackStep));
+		y0 = (float)(radius * sin(alpha0));
+		r0 = (float)(radius * cos(alpha0));
+		y1 = (float)(radius * sin(alpha1));
+		r1 = (float)(radius * cos(alpha1));
+		
+		for (int j = 0; j <= (slice*2); j++) {
+			beta = j * sliceStep;
+			x0 = (float)(r0 * cos(beta));
+			z0 = -(float)(r0 * sin(beta));
+			x1 = (float)(r1 * cos(beta));
+			z1 = -(float)(r1 * sin(beta));
+			coodlist.push_back(x0);
+			coodlist.push_back(y0);
+			coodlist.push_back(z0);
+			coodlist.push_back(x1);
+			coodlist.push_back(y1);
+			coodlist.push_back(z1);
+		}
+	}
+	int size = coodlist.size();
+	ballVertices = new float[size];
+	for (int i = 0; i < size; i++) {
+		ballVertices[i] = coodlist.at(i);
+	}
+	return size;
+};
--- Transformation/Transformation/main.cpp
+++ Transformation/Transformation/main.cpp
@@ -4,17 +4,23 @@
 #include <glew/glfw3.h>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <math.h>
+#include <algorithm>
+#include <vector>
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw_gl3.h"
 #include "Shader.h"
-using glm::vec3;
+#include "BallVertices.h"
 
+using glm::vec3;
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 static void glfw_error_callback(int error, const char* description);
 //设置窗口大小
-const unsigned int SCR_WIDTH = 1000;
-const unsigned int SCR_HEIGHT = 640;
-unsigned int cubeVBO, cubeVAO, cubeIBO;
+const unsigned int SCR_WIDTH = 960;
+const unsigned int SCR_HEIGHT = 700;
+glm::mat4 rotateCube(glm::mat4& Model, bool check, GLfloat speed, GLfloat radians);
+glm::mat4 translateCube(glm::mat4 Model, bool check, GLfloat speed, GLfloat offset);
+glm::mat4 zoomCube(glm::mat4 Model, bool check, GLfloat speed, GLfloat offset);
 
 int main() {
 	glfwSetErrorCallback(glfw_error_callback);
@@ -30,7 +36,7 @@ int main() {
 #ifdef __APPLE__
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //如果使用的是Mac OS X系统，可将上面的代码解除注释
 #endif
-
+	
 	//创建一个窗口对象，这个窗口对象存放了所有和窗口相关的数据，而且会被GLFW的其他函数频繁地用到。
 	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Transformation", NULL, NULL);
 	if (window == NULL) {
@@ -54,22 +60,22 @@ int main() {
 	ImGui_ImplGlfwGL3_Init(window, true);
 	ImGui::StyleColorsDark();
 
-	//创建一个着色器对象
-	Shader cubeShader("shader.vs", "shader.fs");
+	//创建着色器对象
+	Shader cubeShader("cubeShader.vs", "cubeShader.fs");
+	Shader ballShader("ballShader.vs", "ballShader.fs");
 
-	// 设置顶点
+	 //设置顶点
 	GLfloat cubeVertices[] = //画6个四边形,8个顶点
 	{	//位置（x,y,z）		 
-		-0.4f,0.4f, 0.4f, 1.0f,1.0f,0.0f,//0
-		0.4f, 0.4f, 0.4f, 1.0f,1.0f,0.0f,//1
-		0.4f,-0.4f, 0.4f, 0.0f,1.0f,1.0f,//2
-		-0.4f,-0.4f,0.4f, 0.0f,1.0f,1.0f,//3
-		-0.4f,0.4f,-0.4f, 1.0f,0.0f,1.0f,//4
-		0.4f, 0.4f, -0.4f, 1.0f,0.0f,1.0f,//5
-		0.4f,-0.4f,-0.4f, 0.5f,0.0f,1.0f,//6
-		-0.4f,-0.4f,-0.4f, 0.5f,0.0f,1.0f//7
+		-0.2f,0.2f, 0.2f, 1.0f,1.0f,0.0f,//0
+		0.2f, 0.2f, 0.2f, 1.0f,1.0f,0.0f,//1
+		0.2f,-0.2f, 0.2f, 0.0f,1.0f,1.0f,//2
+		-0.2f,-0.2f,0.2f, 0.0f,1.0f,1.0f,//3
+		-0.2f,0.2f,-0.2f, 1.0f,0.0f,1.0f,//4
+		0.2f, 0.2f, -0.2f, 1.0f,0.0f,1.0f,//5
+		0.2f,-0.2f,-0.2f, 0.5f,0.0f,1.0f,//6
+		-0.2f,-0.2f,-0.2f, 0.5f,0.0f,1.0f//7
 	};
-
 	GLuint cubeIndices[] = 
 	{
 		0,1,2,
@@ -86,39 +92,52 @@ int main() {
 		6,7,3  // QUAD6 : bottom
 	};
 
-	glGenVertexArrays(1, &cubeVAO);
-	glBindVertexArray(cubeVAO);
+	GLfloat* ballVertices = new GLfloat[13000];
+	int ballSize = genBallVertices(ballVertices, 0.2, 20, 50);
+	cout << ballSize << endl;
+	unsigned int VBO[2], VAO[2], IBO;
+	glGenVertexArrays(2, VAO);//创建VAO
+	glGenBuffers(2, VBO); //创建VBO	
+	glGenBuffers(1, &IBO); // 创建EBO
 
-	glGenBuffers(1, &cubeVBO); //创建VBO
-	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO); // 绑定新创建的VBO
+	//--------设置立方体----------------------------------------------
+	glBindVertexArray(VAO[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]); // 绑定新创建的VBO
 	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_DYNAMIC_DRAW);
 	//glBindBuffer(GL_ARRAY_BUFFER, 0); // 解除当前绑定的VBO
-
-
-	glGenBuffers(1, &cubeIBO); // 创建EBO
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cubeIBO); // 绑定新创建的EBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO); // 绑定新创建的EBO
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIndices), cubeIndices, GL_DYNAMIC_DRAW); 
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
 	//顶点属性
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	//---------设置球体-------------------------------------------------
+	glBindVertexArray(VAO[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]); // 绑定新创建的VBO
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*ballSize, ballVertices, GL_DYNAMIC_DRAW);
+	//顶点属性
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
 
 	bool show_another_window = true;
 
 	//渲染循环：它能在我们让GLFW退出前一直保持运行
 	while (!glfwWindowShouldClose(window)) {
-		static int radio_flag = 2;
+		static int radio_flag = 1.0f;
 		static GLfloat proj_radians = 45.0f;
 		static GLfloat scale_size = 1.0f;
 		static GLfloat rotate_radians = 0.0f;
 		static int is_filled = 2;
 		static GLfloat translate_x = 0.0f;
-		static glm::vec3 lookat_x = { 0,1,2 };
+		static glm::vec3 lookat_x = { 0,1.5,2 };
 		static glm::vec3 lookat_y = { 0,0,0 };
 		static glm::vec3 lookat_z = { 0,1,0 };
+		static bool t_check = false;
+		static bool r_check = false;
+		static bool s_check = false;
+		static int is_draw_ball = 2;
 
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -132,27 +151,45 @@ int main() {
 			ImGui::TreePop();
 		}
 		if(ImGui::TreeNode("Depth Test")){
-			ImGui::RadioButton("Enable", &radio_flag, 0); ImGui::SameLine();
-			ImGui::RadioButton("Disable", &radio_flag,1);
-			ImGui::TreePop();
-		}
-		if (ImGui::TreeNode("Perspective")) {
-			ImGui::SliderFloat("Projection Angle", &proj_radians,0.0f, 360.0f);
-			ImGui::DragFloat3("Look At x", (GLfloat*)&lookat_x, 0.5f, -30.f,30.f);
-			ImGui::DragFloat3("Look At y", (GLfloat*)&lookat_y, 0.5f,-30.f, 30.f);
-			ImGui::DragFloat3("Look At z", (GLfloat*)&lookat_z, 0.5f, -30.f, 30.f);
+			ImGui::RadioButton("Disable", &radio_flag,0);ImGui::SameLine();
+			ImGui::RadioButton("Enable", &radio_flag, 1);
 			ImGui::TreePop();
 		}
 		if (ImGui::TreeNode("Transform : Manual")) {
-			ImGui::SliderFloat("Translation Offset", &translate_x, -10.0f, 10.0f);
+			ImGui::SliderFloat("Translation Offset", &translate_x, -3.0f, 3.0f);
 			ImGui::SliderFloat("Rotation Angle", &rotate_radians, 0.0f, 360.0f);
-			ImGui::SliderFloat("Scale Factor", &scale_size, 0.0f, 10.0f);
+			ImGui::SliderFloat("Scale Factor", &scale_size, 0.0f, 5.0f);
 			ImGui::TreePop();
 		}
 		if (ImGui::TreeNode("Transform : Automatic")) {
-			//ImGui::RadioButton("(Translation",);
-			//ImGui::RadioButton("Rotation",);			
-			//ImGui::RadioButton("Scaling",);
+			ImGui::Checkbox("Translation", &t_check);
+			ImGui::Checkbox("Rotation", &r_check);			
+			ImGui::Checkbox("Scaling", &s_check);
+			ImGui::TreePop();
+		}
+		if (ImGui::TreeNode("Amazing Animation")) {
+			if (ImGui::RadioButton("Display",&is_draw_ball,1)) {
+				radio_flag = 1.0f;
+				proj_radians = 45.0f;
+				scale_size = 1.0f;
+				rotate_radians = 0.0f;
+				is_filled = 0;
+				translate_x = 0.0f;
+				lookat_x = { 0,2,3 };
+				lookat_y = { 0,-0.2,0 };
+				lookat_z = { 0,1,0 };
+				t_check = false;
+				r_check = true;
+				s_check = false;
+			}
+			ImGui::RadioButton("No Display", &is_draw_ball, 0);
+			ImGui::TreePop();
+		}
+		if (ImGui::TreeNode("Perspective")) {
+			ImGui::SliderFloat("Projection Angle", &proj_radians, 0.0f, 360.0f);
+			ImGui::DragFloat3("Look At x", (GLfloat*)&lookat_x, 0.5f, -30.f, 30.f);
+			ImGui::DragFloat3("Look At y", (GLfloat*)&lookat_y, 0.5f, -30.f, 30.f);
+			ImGui::DragFloat3("Look At z", (GLfloat*)&lookat_z, 0.5f, -30.f, 30.f);
 			ImGui::TreePop();
 		}
 		ImGui::End();
@@ -162,34 +199,41 @@ int main() {
 		glViewport(0, 0, display_w, display_h);
 		ImGui::Render();
 		ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
-
-
-		if (radio_flag == 0) {
-			glEnable(GL_DEPTH_TEST);//启动深度测试
-		}
-		else if (radio_flag == 1) {
-			glDisable(GL_DEPTH_TEST);//关闭深度测试
-		}
 		
-		cubeShader.use();//激活着色器程序	
-		glm::mat4 View = glm::mat4(1.0f), 
-				  Projection = glm::mat4(1.0f),
-				  Model = glm::mat4(1.0f);
-		View = glm::lookAt(lookat_x, lookat_y, lookat_z);//视角
-		Projection = glm::perspective(glm::radians(proj_radians), (GLfloat)SCR_WIDTH / (GLfloat)SCR_HEIGHT, 0.1f, 100.0f);//透视
-		Model = glm::translate(Model, glm::vec3(translate_x, 0.0f, 0.0f));
-		Model = glm::rotate(Model, glm::radians(rotate_radians), glm::vec3(0.0f, 1.0f, 0.0f));//旋转
-		Model = glm::scale(Model, glm::vec3(scale_size, scale_size, scale_size));//缩放
-		glm::mat4 transform = Projection * View * Model;
+		//----启动或关闭深度测试-----------------------------
+		radio_flag ? glEnable(GL_DEPTH_TEST) : glDisable(GL_DEPTH_TEST);		
+		//----设置视角、模型、透射----------------------------
+		glm::mat4 View = glm::mat4(1.0f), Projection = glm::mat4(1.0f), cubeModel = glm::mat4(1.0f), ballModel = glm::mat4(1.0f);
+		View = glm::lookAt(lookat_x, lookat_y, lookat_z);//视角	
+		Projection = glm::perspective(glm::radians(proj_radians), (GLfloat)SCR_WIDTH / (GLfloat)SCR_HEIGHT, 0.1f, 100.0f);//透视		
+		cubeModel = translateCube(cubeModel, t_check, 0.004, translate_x);//1. 平移
+		cubeModel = rotateCube(cubeModel, r_check,60.0f, rotate_radians); //2. 旋转
+		cubeModel = zoomCube(cubeModel, s_check, 0.0025, scale_size);	  //3. 缩放
+		glm::mat4 cubeTransform = Projection * View * cubeModel;
+		
+		//绘制图元1
+		cubeShader.use();
+		glBindVertexArray(VAO[0]);
 		GLint transformID = glGetUniformLocation(cubeShader.ID, "transform");
-		glUniformMatrix4fv(transformID, 1, GL_FALSE, glm::value_ptr(transform));
-
-		//绘制图元
-		glBindVertexArray(cubeVAO);
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glUniformMatrix4fv(transformID, 1, GL_FALSE, glm::value_ptr(cubeTransform));
 		is_filled ? glPolygonMode(GL_FRONT_AND_BACK, GL_FILL): glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); 
 		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
+		//绘制图元2
+		if (is_draw_ball==1) {
+			glm::mat4 ballTransform = glm::mat4(1.0);
+			ballModel = rotateCube(ballModel, true, 50.0f, 0); //2. 旋转
+			ballModel = translateCube(ballModel, false, 0.004, 1.5f);//1. 平移	
+			ballTransform = Projection * View * ballModel;
+			ballShader.use();
+			glBindVertexArray(VAO[1]);
+			int colorLocation = glGetUniformLocation(ballShader.ID, "ourColor");
+			glUniform4f(colorLocation,0.98f, 0.5f, 0.447f, 1.0f);
+			GLint transformID2 = glGetUniformLocation(ballShader.ID, "transform");
+			glUniformMatrix4fv(transformID2, 1, GL_FALSE, glm::value_ptr(ballTransform));
+			//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			glDrawArrays(GL_TRIANGLES, 0, ballSize/3);
+		}
 
 		// 检查并调用事件，交换缓冲
 		glfwSwapBuffers(window);
@@ -197,9 +241,9 @@ int main() {
 	}
 
 	//当渲染循环结束后我们需要正确释放/删除之前的分配的所有资源
-	glDeleteVertexArrays(1, &cubeVAO);
-	glDeleteBuffers(1, &cubeVBO);
-	glDeleteBuffers(1, &cubeIBO);
+	glDeleteVertexArrays(2, VAO);
+	glDeleteBuffers(2, VBO);
+	glDeleteBuffers(1, &IBO);
 	ImGui_ImplGlfwGL3_Shutdown();
 	ImGui::DestroyContext();
 	glfwTerminate();
@@ -215,4 +259,44 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 static void glfw_error_callback(int error, const char* description)
 {
 	fprintf(stderr, "Error %d: %s\n", error, description);
+}
+
+glm::mat4 rotateCube(glm::mat4& Model, bool check, GLfloat speed,GLfloat radians) {
+	if (check) {
+		Model = glm::rotate(Model, glm::radians((GLfloat)glfwGetTime() * speed), glm::vec3(0.0f, 1.0f, 0.0f));
+	}
+	else {
+		Model = glm::rotate(Model, glm::radians(radians), glm::vec3(0.0f, 1.0f, 0.0f));
+	}
+	return Model;
+}
+
+glm::mat4 zoomCube(glm::mat4 Model,bool check, GLfloat speed, GLfloat offset) {
+	if (check) {
+		static float s = 1.0f;
+		static float f = speed;
+		s = s + f;
+		if (s < 0) f = -f;
+		if (s > 5) f = -f; //持续缩放
+		Model = glm::scale(Model, glm::vec3(s, s, s));//缩放
+	}
+	else {
+		Model = glm::scale(Model, glm::vec3(offset, offset, offset));//缩放
+	}
+	return Model;
+}
+
+glm::mat4 translateCube(glm::mat4 Model, bool check, GLfloat speed, GLfloat offset) {
+	if (check) {
+		static float x = 0;
+		static float h = speed;
+		x = x + h;
+		if (x < -2) h = -h;
+		if (x > 2) h = -h; //左右平移
+		Model = glm::translate(Model, glm::vec3(x, 0.0f, 0.0f));
+	}
+	else {
+		Model = glm::translate(Model, glm::vec3(offset, 0.0f, 0.0f));
+	}
+	return Model;
 }
